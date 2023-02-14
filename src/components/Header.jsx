5f1f1fbe56a248814f618da9bc481c867e28d9e1
Devon <devonhampton10@gmail.com>
2023-02-13 21:10:26 -0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => 
+    <div className="App-header">
+        <header>Stranger's Things</header>
+        <nav className="navbar">
+            <a href="index.jsx">Home</a>
+            <a href="posts.jsx">Posts</a>
+            <a href="profile.jsx">Profile</a>
+            <a href="logout.jsx">Log Out</a>
+  </nav>
+    </div>
